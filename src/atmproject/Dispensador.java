8e500b97f16cf31672ae17b8d3c973e05226d3b7
alejo <alejo@192.168.1.6>
2023-03-23 18:57:32 -0600

    ATM

--- src/atmproject/Dispensador.java
+++ src/atmproject/Dispensador.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package atmproject;
 
-/**
- *
- * @author alejo
- */
 public class Dispensador {
+     int efectivoDisponible;
     
+    boolean verificarEfectivo() { return true; }
+    boolean entregarEfectivo(int montoEfectivo) { return true; }
 }
--- src/atmproject/Transaccion.java
+++ src/atmproject/Transaccion.java
@@ -1,9 +1,5 @@
 package atmproject;
 
-/**
- *
- * @author alejo
- */
 public class Transaccion {
     int monto;
     void ejecutar(int monto){
